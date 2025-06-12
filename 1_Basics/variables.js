299c23f1f3bce47e4f31d5c682b9be84abe53d16
StudyBeacon <yogesrai978@gmail.com>
2025-06-12 02:08:29 +0000

    container commit done

---
+++ 1_Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId =144553
+let accountEmail="yogeshrai.afh@gmail.com"
+var accountPassword="12345"
+accountCity="Damak"
+let accountState;
+
+accountEmail="abc@mail.com"
+accountPassword="98989"
+accountCity="Dharan"
+
+
+// accountId=2 not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to use VAR because of
+issue in block scope and functional scope
+*/
